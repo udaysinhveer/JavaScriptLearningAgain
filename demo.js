d2e5b66223e4a03f7a1d043f952c020528993c6a
Udaysinh Veer <udaysinhveer3@gmail.com>
2022-11-09 15:26:50 +0530

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,5 @@
+console.log("Hello");
+console.log("My name is Udaysinh Veer");
+console.info("This is info"); // info meaning information
+console.warn("i am not that bad guy"); // warn meaning warning
+console.error("opps failed to execute");
