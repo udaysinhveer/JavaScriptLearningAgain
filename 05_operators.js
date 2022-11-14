6c4c667cd8436d702d86ad47dfcc2447798ac041
Udaysinh Veer <udaysinhveer3@gmail.com>
2022-11-14 20:24:03 +0530

    operators lecturer

---
+++ 05_operators.js
@@ -0,0 +1,39 @@
+var num1 = 200;
+var num2 = 100;
+var result = num1+num2; // addition
+console.log(`additon of num1 and num2 is : ${result}`);
+
+var result = num1/num2 // division
+console.log(result);
+
+var result = num1%2; // modulo for find reminder 
+console.log(result);
+
+console.log("==================== Compound addition operator =======================================");
+
+var num1 = 5;
+var num2 = 6;
+num1+=num2; // it add num2 value in num1 and assign both value in num1
+console.log(num1);
+
+console.log("==================== Comparison operator =======================================");
+
+var num1 =100;
+var num2 = 50;
+var num3= 50;
+
+console.log(`num1<num2 =`, num1<num2 );
+console.log(`num1>num2 =`, num1>num2 );
+console.log(`num1!=num2 =`, num1!=num2 );
+
+console.log(`num1<=num3 =`, num1<=num3 );
+console.log(`num1>=num3 =`, num1>=num3 );
+
+
+
+
+
+
+
+
+
