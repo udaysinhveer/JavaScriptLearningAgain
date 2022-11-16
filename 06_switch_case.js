0990c8e0dba261a16c57553842c5c4dd6946b089
Udaysinh Veer <udaysinhveer3@gmail.com>
2022-11-16 10:32:23 +0530

    switch case basics

---
+++ 06_switch_case.js
@@ -0,0 +1,59 @@
+// when multiple if case statement then use switch case statement. 
+
+var dayNumber = 6;
+switch (dayNumber) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        break;
+}
+
+console.log("================== execute default case =================");
+// we give invalid input hence it execute default case 
+// break , stop execution process, if we not use  break it execute till break point
+var num = 12;
+switch (num) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid Input");
+        break;
+}
