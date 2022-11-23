cf2f50f84da086fe1402c5d01c86a78087df41bf
Udaysinh Veer <udaysinhveer3@gmail.com>
2022-11-23 11:39:49 +0530

    arrays basics

---
+++ 17_array.js
@@ -0,0 +1,11 @@
+
+var arrayOfNumbers = [1, 3, 7, 8, 10, 9, 7, 3];
+console.log(arrayOfNumbers);
+console.log(`Total number of elements available  in array : ${arrayOfNumbers.length}`);
+
+var accessArray = arrayOfNumbers[2] // access 2nd number index element 
+console.log(accessArray);
+
+// Update array 
+ arrayOfNumbers[2] = 77; // update value 77 instead of 7
+console.log(arrayOfNumbers);
