7b901bfaa81769d51460779a6ffedc92f965a80e
Udaysinh Veer <udaysinhveer3@gmail.com>
2022-11-28 19:58:13 +0530

    Update 17_array.js

--- 17_array.js
+++ 17_array.js
@@ -10,3 +10,17 @@ console.log(accessArray);
 // Update array 
  arrayOfNumbers[2] = 77; // update value 77 instead of 7
 console.log(arrayOfNumbers);
+
+console.log("======================= Resize Array =========================");
+arrayOfNumbers.length= 3 // now array length is 3.
+console.log(arrayOfNumbers.length);
+console.log(arrayOfNumbers);
+
+arrayOfNumbers.length = 10;
+console.log(arrayOfNumbers.length); // now array of length is 10
+console.log(arrayOfNumbers);
+
+// how to empty array
+
+arrayOfNumbers.length = 0;
+console.log(arrayOfNumbers);
