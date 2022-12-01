1be07ae75157991f2395ecd09eb1b68b091f7095
Udaysinh Veer <udaysinhveer3@gmail.com>
2022-12-01 07:20:24 +0530

    Delete demo.js

--- demo.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello");
-console.log("My name is Udaysinh Veer");
-console.info("This is info"); // info meaning information
-console.warn("i am not that bad guy"); // warn meaning warning
-console.error("opps failed to execute");
