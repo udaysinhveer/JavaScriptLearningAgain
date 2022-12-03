2e34d6c82b6171401090170dd6730d644cb28b31
Udaysinh Veer <udaysinhveer3@gmail.com>
2022-12-03 16:39:04 +0530

    IIFE

---
+++ 42_IIFE.js
@@ -0,0 +1,6 @@
+// this function don't need to call, this is self invoking function also called IIFE
+// IIFE - Immediately invoked function expression
+
+(function abc(){
+    console.log("Hey i am IIFE function");
+})();
